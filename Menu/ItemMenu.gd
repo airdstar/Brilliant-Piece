3d0b499d54f6d9d9bfb8bfdb14f58b26840d8cd2
airdstar <sam@theconrads.ca>
2024-09-01 14:49:40 -0300

    Changed visual style of game to be more low-quality (need to remake menus and hud)

--- Menu/ItemMenu.gd
+++ Menu/ItemMenu.gd
@@ -7,6 +7,7 @@ var itemCount : int
 var highlightedItem : int
 var items : Array[ItemResource]
 var physicalItems : Array[Node3D]
+var itemIndicators : Array[TextureRect]
 
 func _process(_delta: float):
 	$TextureRect/Screen.texture = $SubViewport.get_texture()
@@ -42,35 +43,36 @@ func setItems(itemList : Array[ItemResource]):
 	itemCount = items.size()
 	highlightedItem = 0
 	if itemCount > 0:
+		var loopAmount = 1
 		if itemCount == 1:
-			for m in range(3):
-				for n in range(itemCount):
-					var s = ResourceLoader.load(items[n].associatedModel)
-					s = s.instantiate()
-					physicalItems.append(s)
-					$SubViewport/Base/Items.add_child(s)
+			loopAmount = 3
 		elif itemCount == 2:
-			for m in range(2):
-				for n in range(itemCount):
-					var s = ResourceLoader.load(items[n].associatedModel)
-					s = s.instantiate()
-					physicalItems.append(s)
-					$SubViewport/Base/Items.add_child(s)
-		else:
+			loopAmount = 2
+		
+		for m in range(loopAmount):
 			for n in range(itemCount):
 				var s = ResourceLoader.load(items[n].associatedModel)
 				s = s.instantiate()
 				physicalItems.append(s)
 				$SubViewport/Base/Items.add_child(s)
-			
+				if m == 0:
+					var t = TextureRect.new()
+					t.set_texture(load("res://Menu/ItemHighlight.png"))
+					$TextureRect/Screen.add_child(t)
+					t.size = Vector2(4,4)
+					t.position = Vector2(1 + n * 5,1)
 		swapItem()
 
 func swapItem():
 	for n in range(physicalItems.size()):
 		physicalItems[n].position = Vector3(0,200,0)
 	
+	for n in range(itemCount):
+		$TextureRect/Screen.get_child(n).modulate = Color(0.7,0.7,0.7)
+	
 	physicalItems[highlightedItem].position = selectedItemPos
 	physicalItems[highlightedItem].set_rotation_degrees(Vector3(0,-90,0))
+	$TextureRect/Screen.get_child(highlightedItem).modulate = Color(0,0,0)
 	
 	$TextureRect/NameLabel.text = items[highlightedItem].name
 	$TextureRect/StatsLabel.text = items[highlightedItem].desc
